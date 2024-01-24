14c8de6219ec8b818e75307b97ad994ff92bfc11
ekohachi22 <kajo.sulwinski@gmail.com>
2024-01-24 17:39:53 +0100

    change: uvicorn params

--- app/main.py
+++ app/main.py
@@ -12,4 +12,4 @@ app.include_router(model_router, prefix = "/model")
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="localhost", port=8000, reload=True)
+    uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=True)
